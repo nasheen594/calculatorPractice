bf6df573b1faeb2ecb206a9a689309fd9d5293e5
nasheen594 <35035736+nasheen594@users.noreply.github.com>
2023-06-01 13:05:51 -0400

    primary implementation of calculator done

---
+++ Lab3/practice.js
@@ -0,0 +1,21 @@
+class Bag{
+    color=null;
+    length= 50;
+    material= null;
+
+    Bag(){
+
+        this.color=color;
+        this.length=length;
+        this.material=this.material;
+    }
+
+    Bag(doubleTheLength){
+        this.color=color;
+        this.length=length*doubleTheLength;
+        this.material=this.material;
+    }
+}
+
+var bagpack= new Bag();
+var purse= new Bag(0.5);
